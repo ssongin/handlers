2da4989f1f84ec7ed8bc8d62446b5a9b68dbfc67
Stanislav Songin <ssongin@gmail.com>
2025-11-04 20:54:08 +0200

    feat(header): Add automatic content type

--- pkg/inmemorypages/inmemorypages.go
+++ pkg/inmemorypages/inmemorypages.go
@@ -1,11 +1,13 @@
 package inmemorypages
 
 import (
+	"mime"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/ssongin/core"
-	"github.com/ssongin/handlers/pkg/handlerutil"
+	"github.com/ssongin/handlers/pkg/helper"
 	"github.com/ssongin/handlers/pkg/middleware"
 	"github.com/ssongin/handlers/pkg/model"
 )
@@ -18,13 +20,15 @@ func LoadInMemoryStaticPages(inMemory *model.InMemoryMux) string {
 		content, err := os.ReadFile(inMemory.Endpoints[i].Source)
 		core.CheckError("In memory pages: failed to read source file", err, "source", inMemory.Endpoints[i].Source)
 		inMemory.Endpoints[i].Content = content
+
+		inMemory.Endpoints[i].Headers = ensureContentType(inMemory.Endpoints[i].Headers, inMemory.Endpoints[i].Source)
 	}
 
 	for _, endpoint := range inMemory.Endpoints {
 		e := endpoint
 
 		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			handlerutil.SetHeaders(w, e.Headers, inMemory.CommonHeaders)
+			helper.SetHeaders(w, e.Headers, inMemory.CommonHeaders)
 			w.Write(e.Content)
 		})
 
@@ -37,4 +41,28 @@ func LoadInMemoryStaticPages(inMemory *model.InMemoryMux) string {
 
 	core.GetLogger().Info("Created Serve Multiplexer", "name", inMemory.Name, "numHandlers", len(inMemory.Endpoints))
 	return inMemory.Name
+}
+
+func detectMimeType(path string) string {
+	ext := filepath.Ext(path)
+	if ext == "" {
+		return "application/octet-stream"
+	}
+	mimeType := mime.TypeByExtension(ext)
+	if mimeType == "" {
+		return "application/octet-stream"
+	}
+	return mimeType
+}
+
+func ensureContentType(headers []model.Header, source string) []model.Header {
+	if helper.HasHeader(headers, "Content-Type") {
+		return headers
+	}
+	ct := detectMimeType(source)
+	headers = append(headers, model.Header{
+		Name:  "Content-Type",
+		Value: ct,
+	})
+	return headers
 }
